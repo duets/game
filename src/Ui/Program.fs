aa32ea2374019ff19551a25f06606b984e4c4c38
sleepyfran <fgzv@outlook.com>
2021-07-18 23:31:51 +0100

    :hammer: Support live reloading

--- src/Ui/Program.fs
+++ src/Ui/Program.fs
@@ -1,12 +1,30 @@
 namespace Ui
 
 open Avalonia
+open Avalonia.Controls
 open Avalonia.Controls.ApplicationLifetimes
 open Avalonia.FuncUI
+open Avalonia.Media
 open Avalonia.Themes.Fluent
+open Live.Avalonia
 
 type App() =
     inherit Application()
+    
+    interface ILiveView with
+        member _.CreateView(window: Window) =
+            window.Title <- "Duets"
+            window.Width <- 1280.0
+            window.Height <- 720.0
+            window.MinWidth <- 1280.0
+            window.MinHeight <- 720.0
+            window.TransparencyLevelHint <- WindowTransparencyLevel.AcrylicBlur
+            window.Background <- Brushes.Transparent
+            window.ExtendClientAreaToDecorationsHint <- true
+#if DEBUG
+            window.AttachDevTools()
+#endif
+            Shell.MainControl() :> obj
 
     override this.Initialize() =
         this.Styles.Add (FluentTheme(baseUri = null, Mode = FluentThemeMode.Dark))
@@ -14,8 +32,11 @@ type App() =
 
     override this.OnFrameworkInitializationCompleted() =
         match this.ApplicationLifetime with
-        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
-            desktopLifetime.MainWindow <- Shell.MainWindow()
+        | :? IClassicDesktopStyleApplicationLifetime ->
+            let window = new LiveViewHost(this, fun msg -> printfn $"{msg}");
+            window.StartWatchingSourceFilesForHotReloading();
+            window.Show();
+            base.OnFrameworkInitializationCompleted()
         | _ -> ()
 
 module Program =
--- src/Ui/Shell.fs
+++ src/Ui/Shell.fs
@@ -60,25 +60,12 @@ module Shell =
             ]
         ]
 
-    /// Defines the main window of the application which directly interacts
-    /// with Avalonia. We set the required width and height as well as different
-    /// styles for the app itself.
-    type MainWindow() as this =
-        inherit HostWindow()
+    /// Entry point to the application, which loads Elmish and runs the main
+    /// view defined above.
+    type MainControl() as this =
+        inherit HostControl()
 
         do
-            base.Title <- "Duets"
-            base.Width <- 1280.0
-            base.Height <- 720.0
-            base.MinWidth <- 1280.0
-            base.MinHeight <- 720.0
-            base.TransparencyLevelHint <- WindowTransparencyLevel.AcrylicBlur
-            base.Background <- Brushes.Transparent
-            base.ExtendClientAreaToDecorationsHint <- true
-#if DEBUG
-            base.AttachDevTools()
-#endif
-
             Elmish.Program.mkProgram init update view
             |> Program.withHost this
             |> Program.run
